e275bad42cef610eb2b3f4f6718cf19fa02c4a5e
webodan <danx91@hotmail.es>
2018-03-13 23:54:13 +0100

    Update simpleswitcher.sh
    better approach towards moving sink inputs

--- simpleswitcher.sh
+++ simpleswitcher.sh
@@ -6,11 +6,11 @@ gsettings set org.gnome.desktop.interface cursor-size 36 &
 gsettings set org.gnome.desktop.interface scaling-factor 1 &
 gsettings set org.gnome.desktop.interface text-scaling-factor 1.55 &
 xrandr --output DVI-D-0 --off --output HDMI-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-I-1 --off --output DVI-I-0 --off --output DP-1 --off --output DP-0 --off &
-#sleep .5
 pacmd set-default-sink "alsa_output.pci-0000_01_00.1.hdmi-stereo" &
-pacmd list-sink-inputs | grep index | while read line
+for SINK_ID in $(pactl list sink-inputs | sed -n 's/^Sink Input #\([0-9]*\)$/\1/p')
 do
-pacmd move-sink-input `echo $line | cut -f2 -d' '` "alsa_output.pci-0000_01_00.1.hdmi-stereo" &
+    pactl move-sink-input $SINK_ID "alsa_output.pci-0000_01_00.1.hdmi-stereo"
+    pactl move-sink-input $SINK_ID adapter-soft-volume
 done
 }
 
@@ -21,9 +21,10 @@ gsettings set org.gnome.desktop.interface scaling-factor 1 &
 gsettings set org.gnome.desktop.interface cursor-size 24 & 
 xrandr --output DVI-D-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-0 --off --output DVI-I-1 --off --output DVI-I-0 --mode 1024x768 --pos 1920x7 --rotate normal --output DP-1 --off --output DP-0 --off &
 pacmd set-default-sink "alsa_output.pci-0000_03_07.0.analog-stereo" &
-pacmd list-sink-inputs | grep index | while read line
+for SINK_ID in $(pactl list sink-inputs | sed -n 's/^Sink Input #\([0-9]*\)$/\1/p')
 do
-pacmd move-sink-input `echo $line | cut -f2 -d' '` "alsa_output.pci-0000_03_07.0.analog-stereo" &
+    pactl move-sink-input $SINK_ID "alsa_output.pci-0000_03_07.0.analog-stereo"
+    pactl move-sink-input $SINK_ID adapter-soft-volume
 done
 }
 
