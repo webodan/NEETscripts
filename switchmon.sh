5bcd56ad5f38925c87ae1188efc5491e86660337
webodan <danx91@hotmail.es>
2022-11-06 23:49:46 +0100

    Update switchmon.sh

--- switchmon.sh
+++ switchmon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # monitor switch script with variables
 # new version includes checking for connected bluetooth headphones and skips audio switching if connected
-# thanks jon almeida blog post
+
 
 hdmicard=$(pacmd list-sinks | awk '/name:/ {print $0};' | awk '{ print $2}' | sed 's/<//g; s/>//g' | grep hdmi-stereo-extra)
 analogcard=$(pacmd list-sinks | awk '/name:/ {print $0};' | awk '{ print $2}' | sed 's/<//g; s/>//g' | grep analog)
@@ -9,36 +9,6 @@ analogcard=$(pacmd list-sinks | awk '/name:/ {print $0};' | awk '{ print $2}' | 
 dvimonitor=$(xrandr | grep -E " connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/" | grep DP)
 hdmitv=$(xrandr | grep -E " connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/" | grep HDMI)
 
-while [ ! $# -eq 0 ]
-do
-
-	case "$1" in
-	--hdtv)
-		# HDMI TV only
-		xrandr --output $dvimonitor --off --output $hdmitv --primary --mode 1920x1080 -r 60 --pos 0x0 --rotate normal &
-		pacmd set-default-sink $hdmicard
-#		xfconf-query -c xfce4-panel -p /panels/panel-1/size -s 69
-#		xfconf-query -c xfwm4 -p /general/theme -s RedmondXP-hidpi
-#		xfconf-query --channel xsettings --property /Gtk/CursorThemeSize --set 48
-#		xfconf-query -c xsettings -p /Xft/DPI -s 190
-		pactl -- set-sink-volume $hdmicard 100%
-		pacmd set-default-sink $hdmicard
-		exit
-			;;
-		--hdmon)
-
-		# HD Monitor only
-		xrandr --output $dvimonitor --mode 1920x1080 -r 85 --rotate normal --output $hdmitv --off &
-		pacmd set-default-sink $analogcard
-#		xfconf-query -c xfce4-panel -p /panels/panel-1/size -s 59
-#		xfconf-query -c xfwm4 -p /general/theme -s RedmondXP
-#		xfconf-query --channel xsettings --property /Gtk/CursorThemeSize --set 24
-#		xfconf-query -c xsettings -p /Xft/DPI -s 100
-		pactl -- set-sink-volume $analogcard 100%
-		exit
-			;;
-	esac
-done
 
 function HDMITV {
                 xrandr --output $dvimonitor --off --output $hdmitv --mode 1920x1080 -r 60.00 &
